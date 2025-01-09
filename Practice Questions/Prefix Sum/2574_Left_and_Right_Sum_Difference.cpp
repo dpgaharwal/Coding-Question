514580e2ff9dfecec159f797c37d5345e34c5a95
Happy <dpgaharwal@gmail.com>
2025-01-09 22:04:34 +0530

    Practice Question

---
+++ Practice Questions/Prefix Sum/2574_Left_and_Right_Sum_Difference.cpp
@@ -0,0 +1,34 @@
+/*https://leetcode.com/problems/left-and-right-sum-differences*/
+class Solution
+{
+public:
+    vector<int> leftRightDifference(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        // Prefix sum array
+        vector<int> pref(n, 0);
+        pref[0] = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            pref[i] = pref[i - 1] + nums[i];
+        }
+
+        // Suffix sum array
+        vector<int> suff(n, 0);
+        suff[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            suff[i] = suff[i + 1] + nums[i];
+        }
+
+        // Result array
+        vector<int> result(n);
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = abs(pref[i] - suff[i]);
+        }
+
+        return result;
+    }
+};
