f7d1790c40f2bf5e9e3652c0a9e908d616cb4346
Happy <dpgaharwal@gmail.com>
2025-01-10 20:18:13 +0530

    Another approach of DPP Day-3 3rd question

--- TLE_CodingQuestions/DPP & Bonus/Count_Number_of_Nice_Subarray.cpp
+++ TLE_CodingQuestions/DPP & Bonus/Count_Number_of_Nice_Subarray.cpp
@@ -1,3 +1,68 @@
+// Problem Link: https://leetcode.com/problems/count-number-of-nice-subarrays/
+// Approach: Prefix Sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+
+class Solution
+{
+public:
+    int numberOfSubarrays(vector<int> &nums, int k)
+    {
+        int ans = 0;
+        int n = nums.size();
+        map<int, int> mp;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] % 2 == 0)
+                nums[i] = 0;
+            else
+                nums[i] = 1;
+        }
+        mp[0] = 1;
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+            ans += mp[sum - k];
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+    Solution sol;
+    cout << sol.numberOfSubarrays(nums, k) << endl;
+    return 0;
+}
+
+/***********************************************
+
+// Another Approach: Sliding Window
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -35,3 +100,4 @@ public:
         return lessequaltok(nums, k) - lessequaltok(nums, k - 1);
     }
 };
+*/
